9a6f1beea5cf8e59335234ea832cd3e560de957e
Orhay Benaim <orhaybenaim@gmail.com>
2018-03-19 14:15:23 +0200

    implemented mobile touch

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,27 @@ import './App.css';
 import Player from './component/Player';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      x: 0,
+      y: 0
+    }
+  }
+
+  getPosition = (e) =>{
+    e.preventDefault();
+    this.e = e.targetTouches
+    this.setState(()=>{
+      return {x: this.e[0].pageX , y: this.e[0].pageY}
+    });
+    
+  }
   render() {
     return (
 
-      <div id ='gameBoard'>
-        <Player />
+      <div id ='gameBoard' onTouchMove = {this.getPosition}>
+        <Player x = {this.state.x} y = {this.state.y}/>
       </div>
     );
   }
--- src/component/Player.jsx
+++ src/component/Player.jsx
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 
 const smoothing = 15;
 const radius = 30;
+
+
+
 export default class Player extends Component {
 
+
+
+    
     constructor(props){
         super(props);
         this.state = {
@@ -11,34 +17,23 @@ export default class Player extends Component {
             y: 0
         }
 
-        this.x = 0;
-        this.y = 0;
-           
-
 
     }
 
-    
-    componentDidMount(){// once every thing loaded in this component 
-                        //attach a mouse move to the gameboard element
-        document.getElementById('gameBoard').addEventListener('mousemove', (e)=>{
-            this.x = e.clientX;
-            this.y = e.clientY;
-            // same new positions to vars that only exist in this class (this.x , this.y)
-        });
 
-
+    componentDidMount(){
+  
         setInterval( ()=>{//when we create the component start a interval
             this.setState((pervState) =>{//every 32ms state need to be updated
                 if( (this.state.x > 720 - 30 || this.state.y > 720 - 30 ) ||
-                    ( pervState.x === this.x && pervState.y === this.y) ){//if out of range
+                    ( pervState.x === this.props.x && pervState.y === this.props.y) ){//if out of range
                     return {x: pervState.x , y: pervState.y}//set state to pervious state so no change
                 }
                 else{
                     //get distance by 2 points formula distance
-                    let distance = Math.sqrt( Math.pow( pervState.x  - this.x , 2)  + Math.pow(pervState.y - this.y , 2) ) / smoothing;
+                    let distance = Math.sqrt( Math.pow( pervState.x  - this.props.x , 2)  + Math.pow(pervState.y - this.props.y , 2) ) / smoothing;
                     //get angle between 2 points (mouse position and player position)
-                    let angle = Math.atan2(pervState.y - this.y, pervState.x - this.x);
+                    let angle = Math.atan2(pervState.y - this.props.y, pervState.x - this.props.x);
                     //set the new position to the position we heading at
                     return {x: pervState.x -  Math.cos(angle) *distance , y: pervState.y -  Math.sin(angle) *distance }
                 }
@@ -48,9 +43,10 @@ export default class Player extends Component {
            } , 32);
     }
 
+
   render() {
     return (
-                                    // moving the player using css left and top
+            // moving the player using css left and top
         <div className="player" style={ {left: this.state.x - radius, top: this.state.y + radius} }></div>
     )
   }
