c4dc2eef8bd1531106f4ca4651286bc5c232a1fe
Orhay Benaim <orhaybenaim@gmail.com>
2018-03-19 11:00:12 +0200

    mouse movement

--- src/component/Player.jsx
+++ src/component/Player.jsx
@@ -9,28 +9,35 @@ export default class Player extends Component {
             y: 0
         }
 
-            
+        this.x = 0;
+        this.y = 0;
+           setInterval( ()=>{
+            this.setState((pervState) =>{
+                if( (this.state.x > 720 - 30 || this.state.y > 720 - 30 ) ||
+                    ( pervState.x === this.x && pervState.y === this.y) ){
+                    return {x: pervState.x , y: pervState.y}
+                }
+                else{
+                    let angle = Math.atan2(pervState.y - this.y, pervState.x - this.x);
+                    return {x: pervState.x -  Math.cos(angle) *10 , y: pervState.y -  Math.sin(angle) *10 }
+                }
+            })
+           } , 32);
 
 
     }
 
     componentDidMount(){
         document.getElementById('gameBoard').addEventListener('mousemove', (e)=>{
-            this.setState((pervState) =>{
-                if(e.clientX > 720 - 30 || e.clientY > 720 - 30){
-                    return {x: pervState.x , y: pervState.y}
-                }
-                else{
-                    return {x: e.clientX , y: e.clientY}
-                }
-            })
+            this.x = e.clientX;
+            this.y = e.clientY;
             
         });
     }
 
   render() {
     return (
-        <div className="player" style={ {left: this.state.x - 30 , top: this.state.y - 30} }></div>
+        <div className="player" style={ {left: this.state.x , top: this.state.y} }></div>
     )
   }
 };
