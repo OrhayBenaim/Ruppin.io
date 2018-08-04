82712db05df83602edb7b2897b5a93a097f7fadd
Orhay Benaim <orhaybenaim@gmail.com>
2018-08-05 00:50:40 +0300

    .

--- Web Service/WebSite1/WebSite1/App_Code/SQL_Handler.cs
+++ Web Service/WebSite1/WebSite1/App_Code/SQL_Handler.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Data;
 using System.Data.SqlClient;
 using System.Web.Script.Serialization;
+using System.Configuration;
 
 /// <summary>
 /// Summary description for Class1
